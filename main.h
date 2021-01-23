c80c73b6d8e1700e2309cf4ce98ef196a0e916f2
Jinzxc <jzhang10@stuy.edu>
2021-01-22 20:25:10 -0500

    Add header file for main

---
+++ main.h
@@ -0,0 +1,5 @@
+void readin(char *buffer, int len);
+int pass_auth(char *pass, char *input);
+char * pass_find(char *data, char *input);
+char *log_in(char *data, char *input);
+void handle_friends(char *username, char *input);
